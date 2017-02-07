b13625bb349ba714b485a1db3405ad9ebbe0f222
Madhura Bhave <mbhave@pivotal.io>
2017-02-07 15:04:35 -0800

    Add component scanning

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@ComponentScan
 @EnableWebMvc
 public class Application {
 
