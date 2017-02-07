f820fb7cdfee984f630e4ad7b7ae980f39a500fa
Madhura Bhave <mbhave@pivotal.io>
2017-02-07 14:27:13 -0800

    Initial commit

---
+++ src/main/java/example/Application.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+}
