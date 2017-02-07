3f656349556c5ed0bf5c36e97c4c47bb91faa148
Madhura Bhave <mbhave@pivotal.io>
2017-02-07 14:56:47 -0800

    Add EmbeddedServletContainerFactory

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,10 +1,18 @@
 package example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Application {
+
+	@Bean
+	EmbeddedServletContainerFactory servletContainerFactory() {
+		return new TomcatEmbeddedServletContainerFactory();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
