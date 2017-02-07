e6a1e7e12bbe9c101616d154213d359c6b360971
Madhura Bhave <mbhave@pivotal.io>
2017-02-07 15:03:16 -0800

    Extract controller

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -26,16 +24,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
-	}
-
-	@RestController
-	class HelloController {
-
-		@RequestMapping("/")
-		public String hello() {
-			return "hello world";
-		}
-
 	}
 
 }
