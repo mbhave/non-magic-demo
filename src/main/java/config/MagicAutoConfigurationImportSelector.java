d2d35008bca36bb8c2f67f6278b40bbc59ce72cd
Madhura Bhave <mbhave@pivotal.io>
2017-02-14 10:54:06 -0800

    Use spring.factories file

--- src/main/java/config/MagicAutoConfigurationImportSelector.java
+++ src/main/java/config/MagicAutoConfigurationImportSelector.java
@@ -1,14 +1,25 @@
 package config;
 
+import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MagicAutoConfigurationImportSelector implements DeferredImportSelector {
+public class MagicAutoConfigurationImportSelector
+		implements DeferredImportSelector, BeanClassLoaderAware {
+
+	private ClassLoader classLoader;
 
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] { "config.TomcatConfiguration",
-				"config.DispatcherServletConfiguration", "config.MvcConfiguration" };
+		return SpringFactoriesLoader
+				.loadFactoryNames(EnableMagicAutoConfiguration.class, this.classLoader)
+				.toArray(new String[] {});
+	}
+
+	@Override
+	public void setBeanClassLoader(ClassLoader classLoader) {
+		this.classLoader = classLoader;
 	}
 
 }
