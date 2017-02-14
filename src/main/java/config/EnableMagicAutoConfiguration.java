4ab0cec30d496fd2eb1cd12a84d839a4074228c9
Madhura Bhave <mbhave@pivotal.io>
2017-02-14 10:37:09 -0800

    Add autoconfiguration annotation

--- src/main/java/config/EnableMagicAutoConfiguration.java
+++ src/main/java/config/EnableMagicAutoConfiguration.java
@@ -5,14 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Configuration
-@ComponentScan
-@EnableMagicAutoConfiguration
-public @interface MagicApplication {
+@Import({ TomcatConfiguration.class, DispatcherServletConfiguration.class,
+		MvcConfiguration.class })
+public @interface EnableMagicAutoConfiguration {
 
 }
