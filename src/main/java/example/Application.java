2a210f3a9ad73360360fa0474c5f1771a3e65fe4
Madhura Bhave <mbhave@pivotal.io>
2017-02-16 10:11:07 -0800

    Bind properties

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,10 +1,13 @@
 package example;
 
+import config.EnableMagicProperties;
 import config.MagicApplication;
+import config.ServerProperties;
 
 import org.springframework.boot.SpringApplication;
 
 @MagicApplication
+@EnableMagicProperties(ServerProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
