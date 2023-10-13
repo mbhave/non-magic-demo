29679d02b40f46f2dc9c97906b510afc172e83a9
Madhura Bhave <bhavem@vmware.com>
2023-10-13 10:47:45 -0700

    Add a property

--- src/main/java/config/TomcatConfiguration.java
+++ src/main/java/config/TomcatConfiguration.java
@@ -1,5 +1,6 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnAClass(className = "org.apache.catalina.startup.Tomcat")
 public class TomcatConfiguration {
 
+	@Value("${port:8080}")
+	private int port;
+
 	@Bean
 	TomcatServletWebServerFactory servletContainerFactory() {
-		return new TomcatServletWebServerFactory();
+		return new TomcatServletWebServerFactory(this.port);
 	}
 
 }
