9da854eca711c5a4eccde5b906ea47515383ad2b
Madhura Bhave <bhavem@vmware.com>
2023-10-13 10:47:43 -0700

    Enable Webmvc

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class Application {
 
 	@Bean
