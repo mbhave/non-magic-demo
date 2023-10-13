fa1caac605cc2443efe1c42b22c4c1ce935fead3
Madhura Bhave <bhavem@vmware.com>
2023-10-13 10:47:43 -0700

    Add TomcatServletWebServerFactory

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,10 +1,17 @@
 package example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Application {
+
+	@Bean
+	TomcatServletWebServerFactory servletContainerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
