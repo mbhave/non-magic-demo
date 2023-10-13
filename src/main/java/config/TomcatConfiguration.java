0a6a2e3ebd6ed0a75b1c9fc58f436a5dee587a3b
Madhura Bhave <bhavem@vmware.com>
2023-10-13 10:47:44 -0700

    Add separate conditions for tomcat and undertow

--- src/main/java/config/TomcatConfiguration.java
+++ src/main/java/config/TomcatConfiguration.java
@@ -2,14 +2,30 @@ package config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MagicConfiguration
+@Conditional(TomcatConfiguration.OnTomcatCondition.class)
 public class TomcatConfiguration {
 
 	@Bean
 	TomcatServletWebServerFactory servletContainerFactory() {
 		return new TomcatServletWebServerFactory();
+	}
+
+	static class OnTomcatCondition implements Condition {
+
+		@Override
+		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
+					context.getClassLoader());
+		}
+
 	}
 
 }
